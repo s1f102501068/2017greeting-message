ea425727c03d63200c5be54e15e714655d055d2c
s1f102501068 <s1f102501068@iniad.org>
2025-06-23 16:47:51 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -4,3 +4,18 @@ def greet(name):
 
 
 greet('Inoue')
+from datetime import datetime
+
+def greet(name):
+    hour = datetime.now().hour
+    if hour <= 11:
+        message = 'Good morning'
+    elif hour <= 17:
+        message = 'Hello'
+    else:
+        message = 'Good evening'
+    print(message + "," + name +  "-san!")
+
+
+greet("Inoue")
+print("INIAD")
